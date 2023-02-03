7fc95616a766b91dd89aa370dd8f0628dfd6b9d1
DavIdHabeTyan <davidhabetyanm@gmail.com>
2023-02-04 00:05:55 +0400

    added exercises find weight

--- async.js
+++ async.js
@@ -1,3 +1,7 @@
+// input 'яблоко, слон, дуб, лес', 13;
+//output 'лон, ес', 233
+
+
 const getSlicedString = (str) => {
    return str.split(', ').reduce((acc, val, idx) => {
         if(idx % 2 !== 0) {
@@ -29,4 +33,49 @@ const getSomData = (str, numb) => {
 
 getSomData('яблоко, слон, дуб, лес', 13).then(data => console.log(data))
 
-//'лон, ес' 233
+
+
+const obj = {
+    name: 'David',
+    age: 21,
+}
+
+const strObj = JSON.parse(JSON.stringify(obj));
+console.log(strObj)
+
+
+function test(a, b) {
+    if(a % 2 === 0 && b % 2=== 0) {
+        console.log(a * b)
+    }
+    else if (a % 2 !==0 && b % 2 !== 0){
+        console.log(a + b)
+    }
+        console.log(a % 2 ? a : b)
+}
+
+test(2, 3)
+
+
+const food = [
+    {name: 'pasta', wight: 350, price: 60, quantity: 10},
+    {name: 'spagedi', wight: 350, price: 56, quantity: 8},
+    {name: 'spagedi', wight: 400, price: 68, quantity: 16},
+    {name: 'spagedi', wight: 675, price: 139, quantity: 30},
+    {name: 'spagedi', wight: 1600, price: 339, quantity: 8},
+    {name: 'spagedi', wight: 740, price: 159, quantity: 1},
+    {name: 'spagedi', wight: 230, price: 77, quantity: 4},
+    {name: 'spagedi', wight: 230, price: 69, quantity: 5},
+];
+
+const filterFood = food.filter(elem => elem.price > 80)
+    .filter(elem => elem.quantity < 7)
+    .reduce((acc, val) => {
+      return acc +( val.wight * val.quantity);
+
+    },0)
+
+console.log(filterFood)
+
+
+
